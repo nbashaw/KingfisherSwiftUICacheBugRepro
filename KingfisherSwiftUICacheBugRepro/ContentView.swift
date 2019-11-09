b602e49ccc061a0032739cba9f63e0383cf646db
Nathan Baschez <nbashaw@gmail.com>
2019-11-09 01:58:19 -0800

    Initial Commit

---
+++ KingfisherSwiftUICacheBugRepro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KingfisherSwiftUICacheBugRepro
+//
+//  Created by Nathan Bashaw on 11/9/19.
+//  Copyright © 2019 Operator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
